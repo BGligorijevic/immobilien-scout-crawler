055b9ea9bfe976cfe7f955713d33e809ccffc149
Boris Gligorijevic <boris.gligorijevic@prodyna.com>
2016-08-19 13:00:29 +0200

    Private ads are now optional, by default all ads are searched

--- src/main/java/com/prodyna/App.java
+++ src/main/java/com/prodyna/App.java
@@ -1,16 +1,16 @@
 package com.prodyna;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.util.HashSet;
 import java.util.Set;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 /**
  * Crawler for web site www.immobilienscout24.de.
@@ -19,160 +19,171 @@ import org.jsoup.select.Elements;
  */
 public class App {
 
-	private static Set<String> oldApartments = new HashSet<>();
+    private static Set<String> oldApartments = new HashSet<>();
 
-	public static String WEBSITE_ROOT = "https://www.immobilienscout24.de";
+    public static String WEBSITE_ROOT = "https://www.immobilienscout24.de";
 
-	private static String ROOMS_MIN = "1,5";
+    private static String ROOMS_MIN = "2";
 
-	private static String ROOMS_MAX = "2,5";
+    private static String ROOMS_MAX = "3";
 
-	private static String SIZE_MIN = "40";
+    private static String SIZE_MIN = "45";
 
-	private static String SIZE_MAX = "60";
+    private static String SIZE_MAX = "70";
 
-	private static String PRICE_MIN = "600";
+    private static String PRICE_MIN = "700";
 
-	private static String PRICE_MAX = "900";
+    private static String PRICE_MAX = "1100";
 
-	private static String FREQUENCY = "10";
+    private static String FREQUENCY = "10";
 
-	public static void main(String[] args) {
+    private static boolean ONLY_PRIVATE = false;
 
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
 
-		try {
-			System.out.print("Use defaults? Y/n: ");
-			String def = reader.readLine();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-			if (def.equalsIgnoreCase("n")) {
-				System.out.print("Rooms min: ");
-				ROOMS_MIN = reader.readLine();
-				System.out.print("Rooms max: ");
-				ROOMS_MAX = reader.readLine();
+        try {
+            System.out.print("Use defaults? Y/n: ");
+            String def = reader.readLine();
 
-				System.out.print("Size min: ");
-				SIZE_MIN = reader.readLine();
-				System.out.print("Size max: ");
-				SIZE_MAX = reader.readLine();
+            if (def.equalsIgnoreCase("n")) {
+                System.out.print("Rooms min: ");
+                ROOMS_MIN = reader.readLine();
+                System.out.print("Rooms max: ");
+                ROOMS_MAX = reader.readLine();
 
-				System.out.print("Price min: ");
-				PRICE_MIN = reader.readLine();
-				System.out.print("Price max: ");
-				PRICE_MAX = reader.readLine();
+                System.out.print("Size min: ");
+                SIZE_MIN = reader.readLine();
+                System.out.print("Size max: ");
+                SIZE_MAX = reader.readLine();
 
-				System.out.print("How often do I run (minutes): ");
-				FREQUENCY = reader.readLine();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+                System.out.print("Price min: ");
+                PRICE_MIN = reader.readLine();
+                System.out.print("Price max: ");
+                PRICE_MAX = reader.readLine();
 
-		System.out.println("Min rooms: " + ROOMS_MIN);
-		System.out.println("Max rooms: " + ROOMS_MAX);
+                System.out.print("How often do I run (minutes): ");
+                FREQUENCY = reader.readLine();
 
-		System.out.println("Min size: " + SIZE_MIN);
-		System.out.println("Max size: " + SIZE_MAX);
+                System.out.print("Search only private ads? y/N: ");
+                String privateOnly = reader.readLine();
+                if (privateOnly.equalsIgnoreCase("y")) {
+                    ONLY_PRIVATE = true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		System.out.println("Min price: " + PRICE_MIN);
-		System.out.println("Max price: " + PRICE_MAX);
+        System.out.println("Min rooms: " + ROOMS_MIN);
+        System.out.println("Max rooms: " + ROOMS_MAX);
 
-		System.out.println("I will run every " + FREQUENCY + " minutes.");
+        System.out.println("Min size: " + SIZE_MIN);
+        System.out.println("Max size: " + SIZE_MAX);
 
-		startRunner();
-	}
+        System.out.println("Min price: " + PRICE_MIN);
+        System.out.println("Max price: " + PRICE_MAX);
 
-	private static void startRunner() {
-		Thread t = new Thread(new Runnable() {
+        System.out.println("I will run every " + FREQUENCY + " minutes.");
+        System.out.println("I will search only private ads: " + (ONLY_PRIVATE ? "yes" : "no"));
 
-			@Override
-			public void run() {
-				while (true) {
-					try {
-						System.out.println("Working...");
+        startRunner();
+    }
 
-						String rootUrl = constructUrl();
-						Document doc = Jsoup.connect(rootUrl).get();
-						int noPages = getNoPages(doc);
+    private static void startRunner() {
+        Thread t = new Thread(new Runnable() {
 
-						Set<String> newApartments = new HashSet<>();
-						for (int i = 1; i <= noPages; i++) {
-							if (i == 1) {
-								Set<String> firstPageResults = processPage(doc);
-								newApartments.addAll(firstPageResults);
-							} else {
-								String pageUrl = rootUrl.replace("S-T/", "S-T/P-" + i + "/");
-								Set<String> nthPageResults = processPage(pageUrl);
-								newApartments.addAll(nthPageResults);
-							}
-						}
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        System.out.println("Working...");
 
-						if (!oldApartments.isEmpty()) {
-							Set<String> diff = new HashSet<>();
+                        String rootUrl = constructUrl();
+                        Document doc = Jsoup.connect(rootUrl).get();
+                        int noPages = getNoPages(doc);
 
-							for (String newApartment : newApartments) {
-								if (!oldApartments.contains(newApartment)) {
-									diff.add(newApartment);
-								}
-							}
+                        Set<String> newApartments = new HashSet<>();
+                        for (int i = 1; i <= noPages; i++) {
+                            if (i == 1) {
+                                Set<String> firstPageResults = processPage(doc);
+                                newApartments.addAll(firstPageResults);
+                            } else {
+                                String pageUrl = rootUrl.replace("S-T/", "S-T/P-" + i + "/");
+                                Set<String> nthPageResults = processPage(pageUrl);
+                                newApartments.addAll(nthPageResults);
+                            }
+                        }
 
-							if (!diff.isEmpty()) {
-								GmailSender.send(diff);
-							}
-						}
+                        if (!oldApartments.isEmpty()) {
+                            Set<String> diff = new HashSet<>();
 
-						oldApartments = newApartments;
+                            for (String newApartment : newApartments) {
+                                if (!oldApartments.contains(newApartment)) {
+                                    diff.add(newApartment);
+                                }
+                            }
 
-						int milis = 1000 * 60 * Integer.valueOf(FREQUENCY);
-						Thread.sleep(milis);
-					} catch (InterruptedException | IOException | URISyntaxException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		});
+                            if (!diff.isEmpty()) {
+                                GmailSender.send(diff);
+                            }
+                        }
 
-		t.start();
-	}
+                        oldApartments = newApartments;
 
-	private static String constructUrl() {
-		String url = WEBSITE_ROOT + "/Suche/S-T/Wohnung-Miete/Bayern/Muenchen/-/%s-%s/%s-%s/EURO-%s-%s/-/-/-/true";
+                        int milis = 1000 * 60 * Integer.valueOf(FREQUENCY);
+                        Thread.sleep(milis);
+                    } catch (InterruptedException | IOException | URISyntaxException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
-		return String.format(url, ROOMS_MIN, ROOMS_MAX, SIZE_MIN, SIZE_MAX, PRICE_MIN, PRICE_MAX);
-	}
+        t.start();
+    }
 
-	private static int getNoPages(Document doc) {
-		Element ul = doc.getElementById("pageSelection");
-		Elements selects = ul.getElementsByTag("select");
+    private static String constructUrl() {
+        String url = WEBSITE_ROOT + "/Suche/S-T/Wohnung-Miete/Bayern/Muenchen/-/%s-%s/%s-%s/EURO-%s-%s/-/-/-/true";
 
-		return selects.get(0).getElementsByTag("option").size();
-	}
+        return String.format(url, ROOMS_MIN, ROOMS_MAX, SIZE_MIN, SIZE_MAX, PRICE_MIN, PRICE_MAX);
+    }
 
-	public static Set<String> processPage(Document doc) throws IOException {
-		Set<String> results = new HashSet<>();
+    private static int getNoPages(Document doc) {
+        Element ul = doc.getElementById("pageSelection");
+        Elements selects = ul.getElementsByTag("select");
 
-		Element ul = doc.getElementById("resultListItems");
+        return selects.get(0).getElementsByTag("option").size();
+    }
 
-		for (Element li : ul.getElementsByTag("li")) {
+    public static Set<String> processPage(Document doc) throws IOException {
+        Set<String> results = new HashSet<>();
 
-			// skip non-private apartments
-			Elements privately = li.getElementsByAttributeValueContaining("class", "private block");
-			if (privately.isEmpty()) {
-				continue;
-			}
+        Element ul = doc.getElementById("resultListItems");
 
-			Elements articleElements = li.getElementsByAttributeStarting("data-obid");
-			for (Element article : articleElements) {
-				String apartmentId = article.attr("data-obid");
-				results.add(apartmentId);
-			}
-		}
+        for (Element li : ul.getElementsByTag("li")) {
 
-		return results;
-	}
+            // skip non-private apartments
+            if (ONLY_PRIVATE) {
+                Elements privately = li.getElementsByAttributeValueContaining("class", "private block");
+                if (privately.isEmpty()) {
+                    continue;
+                }
+            }
 
-	public static Set<String> processPage(String url) throws IOException {
-		Document doc = Jsoup.connect(url).get();
-		return processPage(doc);
-	}
+            Elements articleElements = li.getElementsByAttributeStarting("data-obid");
+            for (Element article : articleElements) {
+                String apartmentId = article.attr("data-obid");
+                results.add(apartmentId);
+            }
+        }
+
+        return results;
+    }
+
+    public static Set<String> processPage(String url) throws IOException {
+        Document doc = Jsoup.connect(url).get();
+        return processPage(doc);
+    }
 }
--- src/test/java/com/prodyna/AppTest.java
+++ src/test/java/com/prodyna/AppTest.java
@@ -1,31 +1,31 @@
 package com.prodyna;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.HashSet;
 import java.util.Set;
-
-import org.junit.Ignore;
-import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-	/**
-	 * Rigourous Test :-)
-	 *
-	 * @throws URISyntaxException
-	 * @throws IOException
-	 */
-	@Test
-	@Ignore
-	public void testApp() throws IOException, URISyntaxException {
-		Set<String> apartments = new HashSet<>();
-		apartments.add("87355778");
-		apartments.add("87266815");
+    /**
+     * Rigorous Test :-)
+     *
+     * @throws URISyntaxException
+     * @throws IOException
+     */
+    @Test
+    @Ignore
+    public void testApp() throws IOException, URISyntaxException {
+        Set<String> apartments = new HashSet<>();
+        apartments.add("87355778");
+        apartments.add("87266815");
 
-		GmailSender.send(apartments);
-	}
+        GmailSender.send(apartments);
+    }
 }
